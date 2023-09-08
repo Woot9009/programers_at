c38c551567213ba0ec80d49e9c576cae2ac5a3c6
Woot9009 <128044785+Woot9009@users.noreply.github.com>
2023-09-08 15:05:26 +0900

    Add files via upload

--- lv.0/분수의 덧셈.js
+++ lv.0/분수의 덧셈.js
@@ -33,3 +33,22 @@ function solution(numer1, denom1, numer2, denom2) {
     2-2-2. 안나누어 떨어지면 제수를 1 줄이고, 이를 제수가 1이 될 때까지 반복
   2-3. 나누어 떨어진 제수들을 모두 곱한 값이 분자분모의 최대공약수
 */
+
+
+//최대공약수 찾아서 기약분수 만드는 for문 ++ ver
+function simple(a, b) {
+    let key = 1;
+    
+    for(let i=1; i<=Math.min(a, b); i++) {
+        if(a % i == 0 && b % i == 0) {
+            key = i;
+        }
+    }
+}
+
+/*
+1. a/b를 기약분수로 만듦
+  2-1. Math.min()으로 분자 분모 중 작은 수를 찾기
+  2-2. 제수를 1부터 Math.min(a,b)까지 증가시키면서 a와 b 모두 나누어 떨어지는 수를 찾음
+  2-3. 그중 가장 큰 제수가 최대공약수
+*/
